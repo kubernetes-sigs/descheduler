679b02b63592f959445d22693f57144490619fc1
Avesh Agarwal <avagarwa@redhat.com>
2017-07-30 11:53:23 -0400

    Add rescheduler server command implementation.

---
+++ cmd/rescheduler/app/server.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package app implements a Server object for running the rescheduler.
+package app
+
+import (
+	"fmt"
+
+	"github.com/aveshagarwal/rescheduler/cmd/rescheduler/app/options"
+
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+)
+
+// NewReschedulerCommand creates a *cobra.Command object with default parameters
+func NewReschedulerCommand() *cobra.Command {
+	s := options.NewReschedulerServer()
+	cmd := &cobra.Command{
+		Use:  "rescheduler",
+		Long: `The rescheduler evicts pods which may be bound to less desired nodes`,
+		Run: func(cmd *cobra.Command, args []string) {
+			err := Run(s)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+		},
+	}
+
+	s.AddFlags(cmd.Flags())
+	return cmd
+}
+
+func Run(opt *options.ReschedulerServer) error {
+}
