8f4ec5ebaf54f6f3a6e392a606d12274742ecb0e
(Brien Dieterle) <(briend@gmail.com)>
2017-08-05 16:34:45 -0400

    Update doc.go in api and apis v1alpha1 packages to remove open-api tag.

--- pkg/apis/componentconfig/v1alpha1/doc.go
+++ pkg/apis/componentconfig/v1alpha1/doc.go
@@ -16,7 +16,6 @@ limitations under the License.
 
 // +k8s:deepcopy-gen=package,register
 // +k8s:conversion-gen=github.com/aveshagarwal/rescheduler/pkg/apis/componentconfig
-// +k8s:openapi-gen=true
 // +k8s:defaulter-gen=TypeMeta
 
 // Package v1alpha1 is the v1alpha1 version of the rescheduler's componentconfig API
