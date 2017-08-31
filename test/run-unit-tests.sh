b7e166803148acd1038e5be1952cf20288a658f5
Avesh Agarwal <avagarwa@redhat.com>
2017-08-31 11:32:51 -0400

    Update Makefile and unit test script.

--- test/run-unit-tests.sh
+++ test/run-unit-tests.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/sh
+#!/bin/bash
 
 source "$(dirname "${BASH_SOURCE}")/../hack/lib/init.sh"
 
