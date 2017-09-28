f8fd3b94d00dd79f6cc4acdc81ed047b03b0b769
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2017-09-28 08:48:24 -0400

    Changes for node utilization e2e

---
+++ test/e2e/test-e2e.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Copyright 2014 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+KUBE_ROOT=$(dirname "${BASH_SOURCE}")./../vendor/k8s.io/kubernetes
+source "${KUBE_ROOT}/hack/lib/init.sh"
+
+#kube::golang::build_kube_toolchain
+#kube::golang::setup_env
+
+DIR_BASENAME=$(dirname "${BASH_SOURCE}")
+pushd ${DIR_BASENAME}
+
+cleanup() {
+  popd 2> /dev/null
+  kube::etcd::cleanup
+  kube::log::status "performance test cleanup complete"
+}
+
+trap cleanup EXIT
+
+kube::etcd::start
+
+# Running density tests. It might take a long time.
+kube::log::status "e2e test start"
+go test -test.run=. -test.timeout=60m -test.short=false
+kube::log::status "... e2e test finished"
