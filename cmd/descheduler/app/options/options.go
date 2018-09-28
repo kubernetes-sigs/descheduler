53018a7d91eb9503f0f662c0d3557af04c7fd4d3
Aditya Yadav <ayadav@cs.umass.edu>
2018-09-28 19:17:58 -0400

    Commit to restart builds

--- cmd/descheduler/app/options/options.go
+++ cmd/descheduler/app/options/options.go
@@ -58,5 +58,5 @@ func (rs *DeschedulerServer) AddFlags(fs *pflag.FlagSet) {
 	// max-no-pods-to-evict limits the maximum number of pods to be evicted per node by descheduler.
 	fs.IntVar(&rs.MaxNoOfPodsToEvictPerNode, "max-pods-to-evict-per-node", rs.MaxNoOfPodsToEvictPerNode, "Limits the maximum number of pods to be evicted per node by descheduler")
 	// evict-local-storage-pods allows eviction of pods that are using local storage. This is false by default.
-	fs.BoolVar(&rs.EvictLocalStoragePods, "evict-local-storage-pods", rs.EvictLocalStoragePods, "Enables evicting pods using local storage by descheduler")	
+	fs.BoolVar(&rs.EvictLocalStoragePods, "evict-local-storage-pods", rs.EvictLocalStoragePods, "Enables evicting pods using local storage by descheduler.")	
 }
