a702a4807adec209a2392cf8d543461bd8b049a2
Aditya Yadav <ayadav@cs.umass.edu>
2018-09-28 18:44:21 -0400

    Added support to evict pods that use local storage.
    * Added EvictLocalStoragePods cmd parameter. Tests to come later.

--- pkg/apis/componentconfig/types.go
+++ pkg/apis/componentconfig/types.go
@@ -45,4 +45,7 @@ type DeschedulerConfiguration struct {
 
 	// MaxNoOfPodsToEvictPerNode restricts maximum of pods to be evicted per node.
 	MaxNoOfPodsToEvictPerNode int
+
+	// EvictLocalStoragePods allows pods using local storage to be evicted.
+	EvictLocalStoragePods bool
 }
