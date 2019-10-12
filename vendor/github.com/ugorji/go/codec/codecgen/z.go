73b2fb728742e0538413db5949285ed8f7a7c4bb
Mike Dame <mdame@redhat.com>
2019-10-12 11:23:06 -0400

    bump(*): Remove codecgen dependency

--- vendor/github.com/ugorji/go/codec/codecgen/z.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2012-2018 Ugorji Nwoke. All rights reserved.
-// Use of this source code is governed by a MIT license found in the LICENSE file.
-
-package main
-
-const genCodecPath = "github.com/ugorji/go/codec"
--- vendor/github.com/ugorji/go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/ugorji/go
-
-require github.com/ugorji/go/codec v1.1.7
