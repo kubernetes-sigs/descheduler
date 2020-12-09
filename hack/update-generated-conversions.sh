1795479cf0832e361929ae3bdaf3223222223921
Mike Dame <mdame@redhat.com>
2020-12-08 20:53:05 -0500

    Delete everything but index.yaml from gh-pages

--- hack/update-generated-conversions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
-
-go build -o "${OS_OUTPUT_BINPATH}/conversion-gen" "k8s.io/code-generator/cmd/conversion-gen"
-
-${OS_OUTPUT_BINPATH}/conversion-gen \
-		--go-header-file "hack/boilerplate/boilerplate.go.txt" \
-		--input-dirs "${PRJ_PREFIX}/pkg/apis/componentconfig/v1alpha1,${PRJ_PREFIX}/pkg/api/v1alpha1" \
-		--output-file-base zz_generated.conversion
