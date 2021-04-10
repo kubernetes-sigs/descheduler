a848dac3cfbd7a7a862a3e517e4ee9d9567e5faf
Xiang Liu <lx1036@126.com>
2021-04-10 18:54:06 +0800

    Updating policy api version used in pod evictor

--- pkg/descheduler/strategies/node_affinity_test.go
+++ pkg/descheduler/strategies/node_affinity_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	v1 "k8s.io/api/core/v1"
+	policyv1 "k8s.io/api/policy/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
 	core "k8s.io/client-go/testing"
@@ -153,7 +154,7 @@ func TestRemovePodsViolatingNodeAffinity(t *testing.T) {
 
 		podEvictor := evictions.NewPodEvictor(
 			fakeClient,
-			"v1",
+			policyv1.SchemeGroupVersion.String(),
 			false,
 			tc.maxPodsToEvictPerNode,
 			tc.nodes,
