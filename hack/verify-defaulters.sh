df84dc4548ba1d85152f12f72bdaf9ed259279e3
Pravar Agrawal <pravaag1@in.ibm.com>
2021-04-11 19:59:12 +0530

    add verify script for defaulters gen

--- hack/verify-defaulters.sh
+++ hack/verify-defaulters.sh
@@ -6,7 +6,6 @@ set -o pipefail
 
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 DESCHEDULER_ROOT=$(dirname "${BASH_SOURCE}")/..
-mkdir -p "${DESCHEDULER_ROOT}/_tmp"
 _tmpdir="$(mktemp -d "${DESCHEDULER_ROOT}/_tmp/kube-verify.XXXXXX")"
 
 _deschedulertmp="${_tmpdir}"
@@ -16,21 +15,21 @@ git archive --format=tar --prefix=descheduler/ "$(git write-tree)" | (cd "${_des
 _deschedulertmp="${_deschedulertmp}/descheduler"
 
 pushd "${_deschedulertmp}" > /dev/null 2>&1
-go build -o "${OS_OUTPUT_BINPATH}/deepcopy-gen" "k8s.io/code-generator/cmd/deepcopy-gen"
+go build -o "${OS_OUTPUT_BINPATH}/defaulter-gen" "k8s.io/code-generator/cmd/defaulter-gen"
 
-${OS_OUTPUT_BINPATH}/deepcopy-gen \
-                --go-header-file "hack/boilerplate/boilerplate.go.txt" \
-                --input-dirs "./pkg/apis/componentconfig,./pkg/apis/componentconfig/v1alpha1,./pkg/api,./pkg/api/v1alpha1" \
-                --output-file-base zz_generated.deepcopy
+${OS_OUTPUT_BINPATH}/defaulter-gen \
+            --go-header-file "hack/boilerplate/boilerplate.go.txt" \
+            --input-dirs "${PRJ_PREFIX}/pkg/apis/componentconfig/v1alpha1,${PRJ_PREFIX}/pkg/api/v1alpha1" \
+            --extra-peer-dirs "${PRJ_PREFIX}/pkg/apis/componentconfig/v1alpha1,${PRJ_PREFIX}/pkg/api/v1alpha1" \
+            --output-file-base zz_generated.defaults
 popd > /dev/null 2>&1
 
 pushd "${DESCHEDULER_ROOT}" > /dev/null 2>&1
 if ! _out="$(diff -Naupr pkg/ "${_deschedulertmp}/pkg/")"; then
-    echo "Generated deep-copies output differs:" >&2
+    echo "Generated defaulters output differs:" >&2
     echo "${_out}" >&2
-    echo "Generated deep-copies verify failed. Please run ./hack/update-generated-deep-copies.sh"
-    exit 1
+    echo "Generated defaulters verify failed. Please run ./hack/update-generated-defaulters.sh"
 fi
 popd > /dev/null 2>&1
 
-echo "Generated deep-copies verified."
+echo "Generated Defaulters verified."
