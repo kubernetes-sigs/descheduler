64834fe44cf00fe19f58fabb83ad7b08b7e053ba
Harsha Narayana <harshnar@cisco.com>
2023-01-17 22:16:20 +0530

    metrics: enable loop duration and strategy duration metrics

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -52,7 +52,7 @@ var (
 		&metrics.HistogramOpts{
 			Subsystem:      DeschedulerSubsystem,
 			Name:           "descheduler_loop_duration_seconds",
-			Help:           "Time taken to complete a strategy descheduling operation",
+			Help:           "Time taken to complete a full descheduling cycle",
 			StabilityLevel: metrics.ALPHA,
 			Buckets:        []float64{0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 25, 50, 100, 250, 500},
 		}, []string{})
