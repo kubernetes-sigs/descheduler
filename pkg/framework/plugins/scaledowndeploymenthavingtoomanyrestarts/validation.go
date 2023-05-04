d2c447a9a899cef9bfc3de2a762d07a301c80b11
Hoshea Jiang <fgksgf@gmail.com>
2023-05-04 08:33:17 +0000

    stash

---
+++ pkg/framework/plugins/scaledowndeploymenthavingtoomanyrestarts/validation.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package scaledowndeploymenthavingtoomanyrestarts
+
+import (
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// ValidateScaleDownDeploymentHavingTooManyPodRestartsArgs validates ScaleDownDeploymentHavingTooManyPodRestarts arguments.
+func ValidateScaleDownDeploymentHavingTooManyPodRestartsArgs(obj runtime.Object) error {
+	args := obj.(*ScaleDownDeploymentHavingTooManyPodRestartsArgs)
+	// At most one of include/exclude can be set
+	if args.Namespaces != nil && len(args.Namespaces.Include) > 0 && len(args.Namespaces.Exclude) > 0 {
+		return fmt.Errorf("only one of Include/Exclude namespaces can be set")
+	}
+
+	if args.LabelSelector != nil {
+		if _, err := metav1.LabelSelectorAsSelector(args.LabelSelector); err != nil {
+			return fmt.Errorf("failed to get label selectors from strategy's params: %+v", err)
+		}
+	}
+
+	if args.PodRestartThreshold < 1 {
+		return fmt.Errorf("invalid PodsHavingTooManyRestarts threshold")
+	}
+
+	return nil
+}
