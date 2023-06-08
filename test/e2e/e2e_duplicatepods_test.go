6271d51125e9efe69fd9aeb9982c4edd80baa0b4
Amir Alavi <amiralavi7@gmail.com>
2023-06-07 23:32:31 -0400

    update pause image from 'kubernetes/pause' to 'registry.k8s.io/pause'

--- test/e2e/e2e_duplicatepods_test.go
+++ test/e2e/e2e_duplicatepods_test.go
@@ -86,7 +86,7 @@ func TestRemoveDuplicates(t *testing.T) {
 					Containers: []v1.Container{{
 						Name:            "pause",
 						ImagePullPolicy: "Always",
-						Image:           "kubernetes/pause",
+						Image:           "registry.k8s.io/pause",
 						Ports:           []v1.ContainerPort{{ContainerPort: 80}},
 						SecurityContext: &v1.SecurityContext{
 							AllowPrivilegeEscalation: utilpointer.Bool(false),
