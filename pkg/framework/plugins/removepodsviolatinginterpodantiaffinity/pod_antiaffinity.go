d24b20ada5b5e0b755722dcf80405268f8e7bb9b
10hin <10hin@outlook.jp>
2023-10-30 23:28:24 +0900

    apply `gofumpt -l -v -extra .`

--- pkg/framework/plugins/removepodsviolatinginterpodantiaffinity/pod_antiaffinity.go
+++ pkg/framework/plugins/removepodsviolatinginterpodantiaffinity/pod_antiaffinity.go
@@ -19,6 +19,7 @@ package removepodsviolatinginterpodantiaffinity
 import (
 	"context"
 	"fmt"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
