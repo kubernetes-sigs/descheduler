a7082ebae5dd396469dc73b829b5c24f9aa2d57f
Amir Alavi <amiralavi7@gmail.com>
2024-06-07 20:23:58 -0400

    version unit test: use cmp on Info

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -2,43 +2,52 @@ package version
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestGet(t *testing.T) {
 	tests := []struct {
-		name      string
-		version   string
-		wantMajor string
-		wantMinor string
+		name    string
+		version string
+		want    Info
 	}{
 		{
-			name:      "parses automated container release tag",
-			version:   "v20240519-v0.30.0",
-			wantMajor: "0",
-			wantMinor: "30.0",
+			name:    "parses automated container release tag",
+			version: "v20240519-v0.30.0",
+			want: Info{
+				Major:      "0",
+				Minor:      "30.0",
+				GitVersion: "v20240519-v0.30.0",
+			},
 		},
 		{
-			name:      "parses automated container build",
-			version:   "v20240520-v0.30.0-5-g79990946",
-			wantMajor: "0",
-			wantMinor: "30.0",
+			name:    "parses automated container build",
+			version: "v20240520-v0.30.0-5-g79990946",
+			want: Info{
+				Major:      "0",
+				Minor:      "30.0",
+				GitVersion: "v20240520-v0.30.0-5-g79990946",
+			},
 		},
 		{
-			name:      "parses helm release tag",
-			version:   "v20240606-descheduler-helm-chart-0.30.0-18-g8714397b",
-			wantMajor: "0",
-			wantMinor: "30.0",
+			name:    "parses helm release tag",
+			version: "v20240606-descheduler-helm-chart-0.30.0-18-g8714397b",
+			want: Info{
+				Major:      "0",
+				Minor:      "30.0",
+				GitVersion: "v20240606-descheduler-helm-chart-0.30.0-18-g8714397b",
+			},
 		},
 	}
+	ignoreRuntimeFields := cmpopts.IgnoreFields(Info{}, "GoVersion", "Compiler", "Platform")
 	for _, tt := range tests {
 		version = tt.version
 		t.Run(tt.name, func(t *testing.T) {
 			got := Get()
-			if got.Major != tt.wantMajor {
-				t.Errorf("Get() major = got %v, want %v", got.Major, tt.wantMajor)
-			}
-			if got.Minor != tt.wantMinor {
-				t.Errorf("Get() minor = got %v, want %v", got.Minor, tt.wantMinor)
+			if diff := cmp.Diff(got, tt.want, ignoreRuntimeFields); diff != "" {
+				t.Errorf("Get (-want, +got):\n%s", diff)
 			}
 		})
 	}
