9befb6ec17c854bf950c5c0db6808670acb74f0a
Hao Fan <hao.fan@shopee.com>
2024-08-21 17:43:56 +0800

    modify the IMAGE_TAG to ensure that the version is correctly parsed when the descheduler running

--- test/run-e2e-tests.sh
+++ test/run-e2e-tests.sh
@@ -23,7 +23,7 @@ SKIP_INSTALL=${SKIP_INSTALL:-}
 KIND_E2E=${KIND_E2E:-}
 
 # Build a descheduler image
-IMAGE_TAG=$(git describe --tags --match v0*)
+IMAGE_TAG=v$(date +%Y%m%d)-$(git describe --tags)
 BASEDIR=$(dirname "$0")
 VERSION="${IMAGE_TAG}" make -C ${BASEDIR}/.. image
 
